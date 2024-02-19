8b3fbb0dfb61160bd10e02d1a08f99cfbaff71c4
Robert Highbloom <74934232+RHighbloom@users.noreply.github.com>
2024-02-19 01:56:27 -0600

    added anims
    more animations

--- Characters/character-1/character_1.gd
+++ Characters/character-1/character_1.gd
@@ -2,18 +2,19 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 145.0
+const JUMP_VELOCITY = -385.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-enum STATE {IDLE, WALK, JUMP, ROLL}
+##Used to determine which animation to be playing -
+enum STATE {IDLE, WALK, JUMP, ROLL, SLIDE, GETUP}
 enum DIRECTION {LEFT, RIGHT}
 
 var current_state = STATE.IDLE
 var last_direction = DIRECTION.RIGHT
-var landing : bool
 
 
+##Function
 
 
 func set_last_direction(new_direction):
@@ -24,10 +25,6 @@ func set_last_direction(new_direction):
 
 func set_current_state(new_state):
 	if (new_state == current_state):
-		return
-		
-	if (current_state == STATE.JUMP && velocity.y != 0):
-		print("can't change if we're jumping")
 		return
 	
 	match(new_state):
@@ -46,11 +43,19 @@ func set_current_state(new_state):
 				state_machine.travel("jump_left")
 			else:
 				state_machine.travel("jump_right")
+		STATE.SLIDE:
+			if (last_direction == DIRECTION.LEFT):
+				state_machine.travel("slide_left")
+			else:
+				state_machine.travel("slide_right")
+		STATE.GETUP:
+			if (last_direction == DIRECTION.LEFT):
+				state_machine.travel("get_up_left")
+			else:
+				state_machine.travel("get_up_right")
 	current_state = new_state
 
 func _physics_process(delta):
-
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -62,16 +67,20 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_pressed("slide_button"):
+		set_current_state(STATE.SLIDE)
+	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		set_last_direction(direction)
-		velocity.x = direction * SPEED
-		
-		if (velocity.y == 0):
-			set_current_state(STATE.WALK)
-			
+		if (current_state != STATE.SLIDE && current_state != STATE.GETUP):
+			velocity.x = direction * SPEED
+			if (velocity.y == 0):
+				set_current_state(STATE.WALK)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED/50.0)
 	else:
-		if (velocity.y == 0):
+		if (velocity.y == 0 && current_state != STATE.SLIDE && current_state != STATE.GETUP):
 			set_current_state(STATE.IDLE)
 			
 		if (is_on_floor()):
@@ -81,3 +90,9 @@ func _physics_process(delta):
 
 	move_and_slide()
 	#----------------------------------------------------------------------------------------
+
+func _on_animation_tree_animation_finished(anim_name):
+	if (anim_name.contains("slide")):
+		set_current_state(STATE.GETUP)
+	elif (anim_name.contains("get_up")):
+		set_current_state(STATE.IDLE)
